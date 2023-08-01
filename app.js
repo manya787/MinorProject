6f9c11fa3af18e63d76e373d18efc1cfa3e9dd1f
unknown <manyagarg787@gmail.com>
2023-08-01 22:52:41 +0530

    manya

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
+main().catch(err => console.log(err));
+    async function main() {
+          await mongoose.connect('mongodb+srv://manyagarg787:minor@cluster0.qzeqzr2.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true}, {useCreateIndex: true}, {useUnifiedTopology: true}, {useFindAndModify:false});
+        }
+
+// Middlepart
+const middlepart = (req, res, next) => {
+    console.log(`m`);
+    next();
+}
+
 app.get('/', (req, res) => {
+    res.send(`Hello`);
+})
+
+app.get('/search', middlepart, (req, res) => {
+    res.send(`Hello`);
+})
+
+app.get('/post', middlepart, (req, res) => {
+    res.send(`Hello`);
+})
+
+app.get('/signin', (req, res) => {
+    res.send(`Hello`);
+})
+
+app.get('/signup', (req, res) => {
     res.send(`Hello`);
 })
 
